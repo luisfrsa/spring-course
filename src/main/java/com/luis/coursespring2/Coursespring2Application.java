8234e81512a8378d7af26724821cc2a33e6ced35
Luis <luis.alves@db1.com.br>
2018-06-26 18:25:12 -0300

    First commit

---
+++ src/main/java/com/luis/coursespring2/Coursespring2Application.java
@@ -0,0 +1,12 @@
+package com.luis.coursespring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursespring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursespring2Application.class, args);
+	}
+}
